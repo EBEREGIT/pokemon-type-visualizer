ff7b3cc85c904c09fcb222ac133292d7ce41e860
eberegit <samsoneberenjoku@gmail.com>
2024-09-14 00:04:31 +0100

    code optimization

--- src/components/Grid/Half.tsx
+++ src/components/Grid/Half.tsx
@@ -1,13 +1,12 @@
 import Grid from "@mui/material/Grid2";
 import { ReactElement, ReactNode } from "react";
 
-export default function Half({
-  content,
-  sx,
-}: {
+type halfProps = {
   content: ReactElement | ReactNode;
   sx?: object;
-}) {
+};
+
+export default function Half({ content, sx }: halfProps) {
   return (
     <Grid size={{ xs: 12, sm: 12, md: 6, lg: 6 }} sx={{ ...sx }}>
       {content}
--- src/components/Grid/full.tsx
+++ src/components/Grid/full.tsx
@@ -1,12 +1,9 @@
 import Grid from "@mui/material/Grid2";
 import { ReactElement, ReactNode } from "react";
 
-export default function Full(props: {
-  content: ReactElement | ReactNode;
-  sx?: object;
-}) {
-  const { content, sx } = props;
+type fullProps = { content: ReactElement | ReactNode; sx?: object };
 
+export default function Full({ content, sx }: fullProps) {
   return (
     <Grid size={{ xs: 12, sm: 12, md: 12, lg: 12 }} sx={{ ...sx }}>
       {content}
