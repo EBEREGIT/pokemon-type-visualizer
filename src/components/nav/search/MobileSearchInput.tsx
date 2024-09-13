5e4597e323b4c4c166b4be44bff3e96aaa1663fc
eberegit <samsoneberenjoku@gmail.com>
2024-09-13 19:11:31 +0100

    added animation

--- src/components/nav/search/MobileSearchInput.tsx
+++ src/components/nav/search/MobileSearchInput.tsx
@@ -3,17 +3,30 @@ import { ThemeManager } from "../../../stateManager/Theme";
 import { Variable } from "../../../stateManager/variable";
 import SearchInput from "./searchInput";
 import { Box } from "@mui/material";
+import { motion } from "framer-motion";
 
 export default function MobileSearchInput() {
   const { isLessThanMD } = useContext(ThemeManager);
   const { show } = useContext(Variable);
 
+  const showCSS = {
+    opacity: 1,
+    display: "block",
+  };
+
+  const hideCSS = {
+    opacity: 0,
+    display: "none",
+  };
+
   return (
     <>
-      {isLessThanMD && show ? (
-        <Box component={"section"} sx={{mt:2}}>
-          <SearchInput />
-        </Box>
+      {isLessThanMD ? (
+        <motion.div animate={show ? showCSS : hideCSS}>
+          <Box component={"section"} sx={{ mt: 2 }}>
+            <SearchInput />
+          </Box>
+        </motion.div>
       ) : (
         ""
       )}
