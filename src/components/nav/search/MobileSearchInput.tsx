48b09e960c4f68d5022e307fb146221484e4d90d
eberegit <samsoneberenjoku@gmail.com>
2024-09-13 22:29:32 +0100

    updates on Animation

--- src/components/nav/search/MobileSearchInput.tsx
+++ src/components/nav/search/MobileSearchInput.tsx
@@ -3,30 +3,17 @@ import { ThemeManager } from "../../../stateManager/Theme";
 import { Variable } from "../../../stateManager/variable";
 import SearchInput from "./searchInput";
 import { Box } from "@mui/material";
-import { motion } from "framer-motion";
 
 export default function MobileSearchInput() {
   const { isLessThanMD } = useContext(ThemeManager);
   const { show } = useContext(Variable);
 
-  const showCSS = {
-    opacity: 1,
-    display: "block",
-  };
-
-  const hideCSS = {
-    opacity: 0,
-    display: "none",
-  };
-
   return (
     <>
-      {isLessThanMD ? (
-        <motion.div animate={show ? showCSS : hideCSS}>
-          <Box component={"section"} sx={{ mt: 2 }}>
-            <SearchInput />
-          </Box>
-        </motion.div>
+      {isLessThanMD && show ? (
+        <Box component={"section"} sx={{ mt: 2 }}>
+          <SearchInput />
+        </Box>
       ) : (
         ""
       )}
--- src/pages/home/Reports/chart/BarChart.tsx
+++ src/pages/home/Reports/chart/BarChart.tsx
@@ -14,6 +14,7 @@ import Layout from "../../../../components/General/Layout";
 import { useContext } from "react";
 import { Variable } from "../../../../stateManager/variable";
 import { ThemeManager } from "../../../../stateManager/Theme";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function BarChartComponent(props: {
   data: SortedPokemon[];
@@ -24,44 +25,54 @@ export default function BarChartComponent(props: {
   const { theme, mode } = useContext(ThemeManager);
 
   return (
-    <Layout
-      content={
-        <>
-          {/* chart title */}
-          <Heading
-            label={label}
-            typographyStyles={{ fontWeight: 500 }}
-            variant="body1"
-            styles={{ mb: 2 }}
-          />
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={label}
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <Layout
+          content={
+            <>
+              {/* chart title */}
+              <Heading
+                label={label}
+                typographyStyles={{ fontWeight: 500 }}
+                variant="body1"
+                styles={{ mb: 2 }}
+              />
 
-          {/* chart body */}
-          <ResponsiveContainer width={"100%"} height={400}>
-            <BarChart data={data}>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
+              {/* chart body */}
+              <ResponsiveContainer width={"100%"} height={400}>
+                <BarChart data={data}>
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
 
-              {/* display each bar cel with its unique color */}
-              <Bar dataKey={showPercentage ? "percentage" : "count"}>
-                {data.map((entry) => (
-                  <Cell
-                    cursor="pointer"
-                    fill={entry.color}
-                    stroke={
-                      mode
-                        ? theme.palette.primary.dark
-                        : theme.palette.secondary.light
-                    }
-                    key={`cell-${entry}`}
-                  />
-                ))}
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-        </>
-      }
-    />
+                  {/* display each bar cel with its unique color */}
+                  <Bar dataKey={showPercentage ? "percentage" : "count"}>
+                    {data.map((entry) => (
+                      <Cell
+                        cursor="pointer"
+                        fill={entry.color}
+                        stroke={
+                          mode
+                            ? theme.palette.primary.dark
+                            : theme.palette.secondary.light
+                        }
+                        key={`cell-${entry}`}
+                      />
+                    ))}
+                  </Bar>
+                </BarChart>
+              </ResponsiveContainer>
+            </>
+          }
+        />
+      </motion.div>
+    </AnimatePresence>
   );
 }
